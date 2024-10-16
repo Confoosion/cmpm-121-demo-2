bb77cd079c787ae3c879a3f2876ebecd7ae80980
Jake Sales <jfsales@ucsc.edu>
2024-10-15 19:06:35 -0700

    Step 2 Complete
    Simple marker drawing. Added event listeners for the mouse to allow for drawing onto the canvas. Also added a clear button to clear the canvas.

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,55 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
 
+// TITLE
 const title = document.createElement("h1");
 title.innerText = APP_NAME;
 app.appendChild(title);
 
+// DRAWING CANVAS
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 canvas.id = "gameCanvas";
 app.appendChild(canvas);
+
+// CLEAR BUTTON
+const clearButton = document.createElement("button");
+clearButton.innerText = "Clear";
+clearButton.id = "clearButton";
+app.appendChild(clearButton);
+
+const ctx = canvas.getContext("2d");
+let drawing = false;
+
+// DRAW EVENT
+canvas.addEventListener("mousedown", (e) => {
+  drawing = true;
+  ctx?.beginPath();
+  ctx?.moveTo(e.offsetX, e.offsetY);
+});
+
+// CREATE LINE EVENT
+canvas.addEventListener("mousemove", (e) => {
+  if (drawing) {
+    ctx?.lineTo(e.offsetX, e.offsetY);
+    ctx?.stroke();
+  }
+});
+
+// DONE DRAWING EVENT
+canvas.addEventListener("mouseup", () => {
+  drawing = false;
+  ctx?.closePath();
+});
+
+// MOUSE NOT IN CANVAS EVENT
+canvas.addEventListener("mouseleave", () => {
+  drawing = false;
+  ctx?.closePath();
+});
+
+// CLEAR BUTTON EVENT
+clearButton.addEventListener("click", () => {
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
+});
