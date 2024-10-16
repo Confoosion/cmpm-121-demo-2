8a7e2846756e316cac43b27fca711485c9f0723f
Jake Sales <jfsales@ucsc.edu>
2024-10-15 18:52:12 -0700

    Step 1 Complete
    Added initial non-interactive UI layout. There is an app title in an h1 element, and there is a canvas which can be visible through style.css

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,13 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.innerText = APP_NAME;
+app.appendChild(title);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "gameCanvas";
+app.appendChild(canvas);
